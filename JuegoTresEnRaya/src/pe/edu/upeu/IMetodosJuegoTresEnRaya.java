1f583997bd48dc2e0ecc409b76e035e4ec338e53
Alumnos <Alumnos@LAB_SW-PC04>
2016-10-03 08:37:31 -0500

    probando Ejemplo Herencia del Proyecto

---
+++ JuegoTresEnRaya/src/pe/edu/upeu/IMetodosJuegoTresEnRaya.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.edu.upeu;
+public interface IMetodosJuegoTresEnRaya {
+     public void ejecutarJuegoTreEnRaya();
+     public void inicializarTablero();
+     public void imprimirTableroResultado();
+     public boolean comprobarGanador(int valor_jugador);
+     
+       
+
+    /**
+     *@author CesarMc
+     */
+    
+    
+}
